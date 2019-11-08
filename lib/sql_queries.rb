19ea66134c003b4c5b92a2011b8b521f9c620909
chic-page-6502 <zacharytq@gmail.com>
2019-11-08 02:55:26 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -46,7 +46,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-  "SELECT projects.category, pledges.amount FROM projects
+  "SELECT projects.category, SUM(pledges.amount) FROM projects
    JOIN pledges
    ON projects.id = pledges.project_id
    WHERE projects.category = 'books'
