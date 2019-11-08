da0eb8a34222d86d421f7f64c5b83ae6cd0e1051
chic-page-6502 <zacharytq@gmail.com>
2019-11-08 02:30:58 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,7 +14,7 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT users.name, SUM(pledges.amount) FROM users
+  "SELECT users.name, pledges.amount FROM users
    JOIN pledges
    ON users.id = pledges.user_id
    GROUP BY users.name;"
