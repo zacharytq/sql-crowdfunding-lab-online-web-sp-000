704a6b93ec898962489c303c0e983a6a28d60a68
chic-page-6502 <zacharytq@gmail.com>
2019-11-08 02:54:13 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -40,7 +40,7 @@ end
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category, pledges.amount FROM projects
    JOIN pledges
-   ON project.id = pledges.project_id
+   ON projects.id = pledges.project_id
    WHERE projects.category = 'music'
    GROUP BY projects.category;"
 end
